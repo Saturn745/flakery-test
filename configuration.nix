ea07bbd21e45e5ca30a65ce9c37aae84ef792204
Saturn745 <90934664+Saturn745@users.noreply.github.com>
2024-06-21 12:47:50 -0700

    first commit

---
+++ configuration.nix
@@ -0,0 +1,26 @@
+{_}: let
+  flakeryDomain = builtins.readFile /metadata/flakery-domain;
+in {
+  system.stateVersion = "23.05";
+
+  services.nginx = {
+    enable = true;
+    recommendedTlsSettings = true;
+    recommendedOptimizations = true;
+    recommendedGzipSettings = true;
+    recommendedProxySettings = true;
+    virtualHosts."${flakeryDomain}" = {
+      forceSSL = true;
+      enableACME = true;
+      locations."/" = {
+        extraConfig = ''
+          add_header Content-Type text/html;
+        '';
+        return = ''
+          200 "<html><body>Hello from Flakery</body></html>
+        '';
+      };
+    };
+  };
+  networking.firewall.allowedTCPPorts = [80 443];
+}
